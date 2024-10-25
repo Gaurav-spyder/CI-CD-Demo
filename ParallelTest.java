63286ed0e88f46b6e6b616ad1ad26d3c7be76146
Gaurav-spyder <g.guptabih@gmail.com>
2024-10-25 18:21:20 +0530

    First Commit

---
+++ ParallelTest.java
@@ -0,0 +1,39 @@
+package testngDemo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ParallelTest {
+	
+
+	WebDriver driver;
+	@Test
+	public void testChrome() {
+		 driver=new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+			driver.manage().window().maximize();
+			driver.get("https://www.saucedemo.com/v1/");
+			System.out.println(Thread.currentThread().getId());
+	}
+	@Test
+public void testFirefox() {
+	 driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/v1/");
+		System.out.println(Thread.currentThread().getId());
+	}
+	@Test
+public void testIE() {
+	 driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		System.out.println(Thread.currentThread().getId());
+	}
+}
